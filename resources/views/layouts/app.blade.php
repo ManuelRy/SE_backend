0fc4810703df4793ddce6c46aee4b7a544c50b0c
ManuelRy <121281145+ManuelRy@users.noreply.github.com>
2024-07-05 23:21:32 +0700

    new

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,7 @@
                 <li class="mb-2">
                     <a href="{{ route('users.index') }}" class="nav-link {{ request()->is('users') ? 'active' : '' }}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5V4H2v16h5m10-4a5 5 0 11-10 0 5 5 0 0110 0z" />
+                            <path d="M12 12c2.209 0 4-1.791 4-4s-1.791-4-4-4-4 1.791-4 4 1.791 4 4 4zm0 2c-2.673 0-8 1.334-8 4v2h16v-2c0-2.666-5.327-4-8-4zm8.5 5.668c.837-.47 1.5-1.35 1.5-2.335 0-1.573-1.5-2.625-3.417-3.182-.304.15-.65.265-1.033.342.007.022.012.045.019.068 2.214.782 3.431 1.742 3.431 2.772 0 .9-.687 1.675-1.718 2.079.053.083.111.158.158.256.032.063.049.13.07.199zm-16 0c.021-.069.038-.136.07-.199.047-.098.105-.173.158-.256-1.031-.404-1.718-1.179-1.718-2.079 0-1.03 1.217-1.99 3.431-2.772.007-.023.012-.046.019-.068-.383-.077-.729-.192-1.033-.342-1.917.557-3.417 1.609-3.417 3.182 0 .985.663 1.865 1.5 2.335.058.032.118.055.178.083.03.014.061.022.092.033.036.013.072.029.109.041z"/>
                         </svg>
                         Users
                     </a>
@@ -81,7 +81,7 @@
                 <li class="mb-2">
                     <a href="{{ route('lockers.index') }}" class="nav-link {{ request()->is('lockers') ? 'active' : '' }}">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17V7a3 3 0 116 0v10a3 3 0 11-6 0z" />
+                            <path d="M8 2h8a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2m0 2v16h8V4zm2 9h2v4h-2zm0-7h4v1.5h-4zm0 3h4v1.5h-4z" />
                         </svg>
                         Lockers
                     </a>
